9a96f56a6658d7c458d5e1bd8f1089b6a2552518
demikhovr <demikhovr@gmail.com>
2019-02-23 20:47:52 +0300

    add make-task.js and make-filter.js modules

---
+++ src/make-filter.js
@@ -0,0 +1,16 @@
+export default ({
+  caption,
+  amount = 0,
+  isChecked = false,
+}) => `<input
+    type="radio"
+    id="filter__${caption.toLowerCase()}"
+    class="filter__input visually-hidden"
+    name="filter"
+    ${isChecked ? ` checked` : ``}
+    ${!amount ? ` disabled` : ``}
+  />
+  <label for="filter__${caption.toLowerCase()}" class="filter__label">
+    ${caption.toUpperCase()}
+    <span class="filter__${caption.toLowerCase()}-count">${amount}</span>
+  </label>`;
